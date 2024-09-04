a1a89bb933237a91c43a75c06753f38301e1c170
Avi <avis2912@gmail.com>
2024-09-04 00:54:37 -0500

    max-width fix

--- functions/src/publishBlog.js
+++ functions/src/publishBlog.js
@@ -3,7 +3,8 @@ import axios from 'axios';
 async function publishBlog(user_id, user_app_password, siteUrl, htmlContent, titleTag, scheduledFor=null) {
 
     const article_title = htmlContent.match(new RegExp(`<${titleTag}>(.*?)</${titleTag}>`))[1];
-    const formattedContent = htmlContent.replace(new RegExp(`<${titleTag}>.*?</${titleTag}>`, 'g'), '');
+    const formattedContent = htmlContent.replace(new RegExp(`<${titleTag}>.*?</${titleTag}>`, 'g'), '').replace(/<p>\s*(<img[^>]*>)\s*<\/p>/g, '$1');
+    const formattedContent1 = formattedContent.replace(/(<img[^>]*)(>)/g, '$1 style="max-width: 100%;"$2');
     const formattedSiteUrl = siteUrl.replace('https://', '').replace('http://', '');
     const wp_url = `https://${formattedSiteUrl}/wp-json/wp/v2`;
     const wp_post_url = wp_url + "/posts";
@@ -11,13 +12,18 @@ async function publishBlog(user_id, user_app_password, siteUrl, htmlContent, tit
     const credentials = btoa(user_id + ':' + user_app_password);
     const header = { 'Authorization': 'Basic ' + credentials };
 
+    console.log('HTML:', formattedContent1);
+
     const post_data = {
         "title": article_title,
-        "content": htmlContent,
-        "comment_status": "closed",
+        "content": formattedContent1,
         "categories": [1],
         "status": scheduledFor ? 'future' : 'publish',
         ...(scheduledFor && { "date": '2024-08-31T12:00:00' }),
+        // "title": 'article_title',
+        // "content": `<p>hey</p>  
+        // <img src="https://cdn.audleytravel.com/3551/2537/79/1333985-na-pali-coastline--kauai-hawaii.jpg" alt="image" style="max-width: 100%" />`,
+        // "comment_status": "closed",
         // "featured_media": 0,
     };
 
